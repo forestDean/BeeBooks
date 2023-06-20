795a1f5865ef61ef01fe596a90c8f6e4f7a26f79
forestDean <113769986+forestDean@users.noreply.github.com>
2023-06-20 16:07:35 +0100

    Initial commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// Invoke the `configure` method
+configure(
+    // pass it an object
+    {
+      // The Object has a key titled `adapter` and a value that equals a new instantiated instance of the `Adapter` class.
+      adapter: new Adapter()
+    }
+)  
