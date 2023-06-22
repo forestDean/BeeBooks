1989f60f4b0556ef9cca3ad14437b3deeb3adcab
milla56 <lubomila14@gmail.com>
2023-06-23 00:25:27 +0100

    company values

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,6 +3,9 @@ import '../About/about.css'
 
 import { Container} from "react-bootstrap";
 import book from '../../assets/images/image4.png'
+import child1 from '../../assets/images/child1.png'
+import child2 from '../../assets/images/child2.png'
+import child3 from '../../assets/images/child3.png'
 
 
 const About = () => {
@@ -46,9 +49,65 @@ storytelling and a desire to ignite the spark of imagination in every child's he
       </div>
       </Container>
       </div>
+         
+
+
+      <h3 className="valueT"> Our Values</h3>
+      <div className="cards">
+     
+        
+
+
+        <div className="card" >
+        <div className="img-box">
+        <img className='book' src={child1} alt="child"/>
+        </div>
+        <div className="content">
+            <h2>Imagination</h2>
+            <p>
+            We believe in the limitless power of imagination. 
+            We encourage every child to dream big and explore the boundless possibilities that 
+            lie within the pages of a book.
+            </p>
+        
+        </div>
+    </div>
+
+    {/* CARD2 */}
+<div className="card" >
+  <div className="img-box">
+  <img className='child' src={child2} alt="child"/>
+  </div>
+  <div className="content">
+      <h2>Education</h2>
+      <p>
+       We strive to provide 
+      stories that not only entertain but also teach valuable life lessons, 
+      instilling core values such as kindness, empathy, and resilience in our young readers.
+      </p>
+  </div>
+</div>
+
+
+{/* CARD3 */}
+<div className="card" >
+  <div className="img-box">
+  <img className='child' src={child3} alt="child"/>
+  </div>
+  <div className="content">
+      <h2>Joy</h2>
+      <p>
+        We aim to create an environment that sparks laughter, excitement, and delight, ensuring that every 
+        story we present is a source of happiness and inspiration.
+      </p>
+  </div>
+</div>
+</div>
+</div>
+     
 
       
-      </div>
+   
 
 
 )
