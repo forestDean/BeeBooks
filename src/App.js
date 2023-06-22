b4aa0be95d2422add5e9fb1a478bbc4dccbce3b9
forestDean <113769986+forestDean@users.noreply.github.com>
2023-06-22 11:41:49 +0100

    staging page & router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.jpg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Staging from './pages/Staging';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
+import Container from "./components/Container";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
-}
+      <Router>
+        <Container>
+          <Header />
+          <Wrapper>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              {/* <Route path="/13-react-portfolio/" element={<Home/>} /> */}
+              <Route path="/pages/Staging" element={<Staging/>} />
+            </Routes>
+          </Wrapper>
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
 
-export default App;
+export default App
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./style.css";
+import Row from "../../components/Row";
+
+class Footer extends Component {
+  render() {
+      return (
+        <footer className="footer">
+          <Row>
+            <small className="mt-3 text-center">©2023 SuperCool</small>
+          </Row>
+        </footer>
+      );
+    }
+  }
+
+export default Footer;
