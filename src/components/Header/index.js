6d87719f3201dd3a0ace1042756b4d3c1cbc1bca
milla56 <lubomila14@gmail.com>
2023-06-22 20:05:17 +0100

    navbar changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,37 @@
 import React from "react";
 import "./header.css";
-import Row from "../../components/Row";
-import Column from "../../components/Column";
+// import Row from "../../components/Row";
+// import Column from "../../components/Column";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+// LOGO DESIGN
+// import logo from '../../assets/images/beelogo.png';
 
 
 const Header = () => {
+return (  
+    <Navbar expand="lg" className="bg-body-tertiary">
 
+    <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 
-    return (  
-        <header>    
-            <Row>
-                <Column size="md-12">
-                    <h4 className="mr-3 font-weight-light">text</h4>
-                </Column>
-            </Row>
-            <Row>
-                <Column size="md-12">
-                    <h1>BeeBooks</h1>
-                </Column>
-            </Row>
-        </header>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#contact">Contact Us</Nav.Link>
+
+         
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
         );
 }
 
