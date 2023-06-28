b76868e78a2f6f8984fe4913cc4b3b8f4bcb8fed
forestDean <113769986+forestDean@users.noreply.github.com>
2023-06-28 14:39:27 +0100

    initial stage of expanding BookCard

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -1,5 +1,6 @@
 import Column from "../Column";
 import "./bookcard.css";
+import React, { useState } from 'react'
 
 const BookCard = (props) => {
 
@@ -11,15 +12,24 @@ const BookCard = (props) => {
         window.open(url, '_blank');
     };
 
+    // function HiddenText() {
+        const [isVisible, setIsVisible] = useState(false);
+      
+        const revealText = () => {
+          setIsVisible(!isVisible);
+        };
+      
+    // }
     return (
 
     <Column size="lg-6" id="bookInfo">
         <article className="bookCard text-center mb-5" id={props.id}>
-            <div className="summary">
+            <div className={`hidden ${isVisible ? 'visible' : ''}`}>
                 <h3>{props.title}</h3>
                 <p className="author">by {props.author}</p>
                 {props.image}
                 <p id="description" className="fst-italic">{props.description}</p>
+                <button className="fixed-bottom" onClick={revealText}>Click to reveal</button>
             </div>
             <small className="fixed-bottom" id="isbn">ISBN: {props.isbn}</small>
             <button type="button" id="shopButton" onClick={goShop} className="btn outline mb-4 fixed-bottom">SHOP</button>
@@ -28,7 +38,8 @@ const BookCard = (props) => {
 
     );
 
+    
+
 }
 
 export default BookCard;
-
