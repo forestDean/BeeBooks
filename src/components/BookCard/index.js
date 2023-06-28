525399e0ea656bd6cc593f57f4b0a65833a39b47
forestDean <113769986+forestDean@users.noreply.github.com>
2023-06-28 13:50:25 +0100

    prevent page refresh on SHOP button

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -1,24 +1,26 @@
 import Column from "../Column";
 import "./bookcard.css";
 
-
-
 const BookCard = (props) => {
 
-    const goShop = () => {
-    console.log('props @Google',  props.isbn)
-    const url = `https://www.google.com/search?q=${encodeURIComponent(props.isbn)}`;
-    window.location= url;
+    const goShop = (event) => {
+        event.preventDefault();
+        console.log('props @Google',  props.isbn)
+        const url = `https://www.google.com/search?q=${encodeURIComponent(props.isbn)}`;
+        // window.location.href = url;
+        window.open(url, '_blank');
     };
 
     return (
 
     <Column size="lg-6" id="bookInfo">
         <article className="bookCard text-center mb-5" id={props.id}>
-            <h3>{props.title}</h3>
-            <p className="author">by {props.author}</p>
-            {props.image}
-            <p id="description" className="fst-italic">{props.description}</p>
+            <div className="summary">
+                <h3>{props.title}</h3>
+                <p className="author">by {props.author}</p>
+                {props.image}
+                <p id="description" className="fst-italic">{props.description}</p>
+            </div>
             <small className="fixed-bottom" id="isbn">ISBN: {props.isbn}</small>
             <button type="button" id="shopButton" onClick={goShop} className="btn outline mb-4 fixed-bottom">SHOP</button>
         </article>
@@ -29,3 +31,4 @@ const BookCard = (props) => {
 }
 
 export default BookCard;
+
