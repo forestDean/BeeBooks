b51328c45376594195b9aecb8645a569cd6caa54
forestDean <113769986+forestDean@users.noreply.github.com>
2023-07-10 18:09:42 +0100

    bugfix mobile display

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -3,7 +3,9 @@ import React from "react";
 function Column(props) {
   const size = props.size.split(" ").map(size => "col-" + size).join(" ");
   const { className } = props;
-  const colClass = `${size} ${className}`
+  // if className has no value return just column size
+  const colClass = className ? `${size} ${className}` : `${size}`;
+
   return <div className={colClass}>{props.children}</div>;
 }
 
